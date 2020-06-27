4a90a837beccfb1275cb49b00acbb6b024435a24
AdamSwice <65133423+AdamSwice@users.noreply.github.com>
2020-06-27 14:15:53 -0400

    Algo fonctionnel

--- src/MotsCaches.java
+++ src/MotsCaches.java
@@ -12,15 +12,17 @@ public class MotsCaches {
     }
 
 
-    private Set<String> wordFinder(){
-        HashSet<String> wordsFound = new HashSet<>();
-        HashSet<String> possibleWords = findPossibleWords();
-
+    public Set<String> wordFinder(){
         int minimumWordLength = Integer.MAX_VALUE;
         for(String words : dictio) {
             if(words.length() < minimumWordLength)
                 minimumWordLength = words.length();
         }
+        
+        HashSet<String> wordsFound = new HashSet<>();
+        HashSet<String> possibleWords = findPossibleWords(minimumWordLength);
+
+        
 
         for (String word : dictio){
             for (String possibleWord : possibleWords){
@@ -33,9 +35,9 @@ public class MotsCaches {
         return wordsFound;
     }
 
-    private HashSet<String> findPossibleWords(){
+    private HashSet<String> findPossibleWords(int minimumWordLength){
         int gridSize = grid.length;
-        Set<String> words = new HashSet<>();
+        HashSet<String> words = new HashSet<>();
 
         //Chaque ligne
         for (int i = 0; i < gridSize; i++){
@@ -45,11 +47,43 @@ public class MotsCaches {
         //Chaque colonne
         for (int i = 0; i < gridSize; i++){
             StringBuffer stringBuilder = new StringBuffer();
-            for (int j = 0; j < gridSize;)
-
+            for (int j = 0; j < gridSize; j++){
+                stringBuilder.append(grid[j][i]);
+            }
+            words.add(stringBuilder.toString());
         }
 
-        return null;
+        //Diagonales principale \
+        StringBuffer diagonalStringBuilder1 = new StringBuffer();
+        StringBuffer diagonalStringBuilder2 = new StringBuffer();
+
+        for (int i = 0; i < gridSize; i++){
+            diagonalStringBuilder1  = diagonalStringBuilder1.append(grid[i][i]);
+            diagonalStringBuilder2 = diagonalStringBuilder2.append(grid[i][gridSize-i-1]);
+        }
+        words.add(diagonalStringBuilder1.toString());
+        words.add(diagonalStringBuilder2.toString());
+
+        //Diagonale principale inverse /
+        for (int i = 0; i < gridSize - minimumWordLength; i++){
+            StringBuffer tempStringBuffer1 = new StringBuffer();
+            StringBuffer tempStringBuffer2 = new StringBuffer();
+            StringBuffer tempStringBuffer3 = new StringBuffer();
+            StringBuffer tempStringBuffer4 = new StringBuffer();
+
+            for (int j = i, k = 0; j < gridSize && k < gridSize; j++, k++){
+                tempStringBuffer1.append(grid[j][k]);
+                tempStringBuffer2.append(grid[k][j]);
+                tempStringBuffer3.append(grid[gridSize - j - 1][k]);
+                tempStringBuffer4.append(grid[gridSize - k - 1][j]);
+            }
+            words.add(tempStringBuffer1.toString());
+            words.add(tempStringBuffer2.toString());
+            words.add(tempStringBuffer3.toString());
+            words.add(tempStringBuffer4.toString());
+        }
+
+        return words;
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 public class main {
     private static char[][] grid;
@@ -13,11 +16,22 @@ public class main {
             grid = file.getGrid();
             dictio = file.getList();
             MotsCaches motsCaches = new MotsCaches(dictio, grid);
+            Set<String> wordsFound = motsCaches.wordFinder();
+
+            List<String> sortedWordList = new ArrayList<>(wordsFound);
+            Collections.sort(sortedWordList);
+
+            for (String word : sortedWordList){
+                System.out.println(word);
+            }
+            int totalWordsFound = wordsFound.size() + 1;
+            System.out.println("Nombre de mots trouvés dans la grille: " + totalWordsFound);
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println((double)(System.nanoTime()-start)/1000000000);
+        System.out.println("Temps d'execution: " + (double)(System.nanoTime()-start)/1000000000);
     }
 
 
